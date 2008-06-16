705a673d7116c8c0fc338ff299f97471539ed2b8
juan <juan@c1298320-b2f5-4960-9a49-8238b2cebc8a>
2008-06-16 21:22:30 +0000

    missing files (subscription element)
    git-svn-id: file:///tmp/foo/su-smack/branches/24@6 c1298320-b2f5-4960-9a49-8238b2cebc8a

--- src/test/java/se/su/it/smack/packet/XMPPElementFactoryTest.java
+++ src/test/java/se/su/it/smack/packet/XMPPElementFactoryTest.java
@@ -1,6 +1,17 @@
 package se.su.it.smack.packet;
 
+import java.io.InputStreamReader;
+import java.util.List;
+
 import junit.framework.TestCase;
+
+import org.xmlpull.mxp1.MXParser;
+import org.xmlpull.v1.XmlPullParser;
+
+import se.su.it.smack.provider.PubSubProvider;
+import se.su.it.smack.pubsub.PubSub;
+import se.su.it.smack.pubsub.elements.SubscriptionElement;
+import se.su.it.smack.pubsub.elements.SubscriptionsElement;
 
 public class XMPPElementFactoryTest extends TestCase {
 
@@ -14,21 +25,16 @@ public class XMPPElementFactoryTest extends TestCase {
 		assertEquals(element.getClass().getName(), "se.su.it.smack.pubsub.elements.SubscribeOptionsElement");
 	}
 
-}
-package se.su.it.smack.packet;
-
-import junit.framework.TestCase;
-
-public class XMPPElementFactoryTest extends TestCase {
-
-	public void testXMPPElementFactoryCreate() throws Exception {
-
-		String name = "subscribe-options";
-
-		XMPPElement element = XMPPElementFactory.create(name);
-
-		assertNotNull(element);
-		assertEquals(element.getClass().getName(), "se.su.it.smack.pubsub.elements.SubscribeOptionsElement");
-	}
-
+	public void testSubscriptions() throws Exception {
+	    final XmlPullParser pp = new MXParser();
+	    pp.setInput(new InputStreamReader(getClass().getResourceAsStream(
+	            "subscriptions.xml")));
+	    final PubSubProvider psp = new PubSubProvider();
+	    final PubSub pubsub = (PubSub) psp.parseIQ(pp);
+	    final SubscriptionsElement subscriptions = (SubscriptionsElement) pubsub.getChildren().get(0);
+	    final List<SubscriptionElement> lists = subscriptions.getChildren();
+	    assertEquals(7, lists.size());
+        pubsub.getChildren();
+	    
+    }
 }
